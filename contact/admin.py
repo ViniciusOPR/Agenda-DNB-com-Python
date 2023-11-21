892e6a7e3244b9b15b2ae9a8dcac2b2322c5953c
Vinicius Ouro Preto Ribeiro <viniciusouro73@gmail.com>
2023-11-21 11:14:36 -0300

    Trabalhando com models

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
+from contact import models
+
 # Register your models here.
+
+@admin.register(models.Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = 'id', 'AKA', 'name', 'phone',
+    ordering = '-id',
+    # list_filter = 'created_date',
+    search_fields = 'id', 'AKA', 'name',
+    list_per_page = 10
+    list_max_show_all = 25
+    list_editable = 'AKA', 'name',
+    list_display_links = 'id', 'phone',
