cc4c588530bb4d18edeed59471763628b0708045
Vinicius Ouro Preto Ribeiro <viniciusouro73@gmail.com>
2023-11-22 12:07:55 -0300

    Modificando o projeto, criando owner, contact_views, etc

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
 
@@ -10,6 +11,10 @@ from django.utils import timezone
 # picture (imagem)
 
 class Category(models.Model):
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
     name = models.CharField(max_length=50)
 
     def __str__(self) -> str:
@@ -25,6 +30,7 @@ class Contact(models.Model):
     show = models.BooleanField(default=True)
     picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/')
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True)
+    owner = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True)
     
 
     def __str__(self) -> str:
