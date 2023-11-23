46c16f4e7e996971e68ed104476baba1564f5379
Vinicius Ouro Preto Ribeiro <viniciusouro73@gmail.com>
2023-11-23 11:20:41 -0300

    Criando Partials e filtrando consultas na agenda

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,15 +1,34 @@
-from django.shortcuts import get_object_or_404, render
+from django.db.models import Q
+from django.shortcuts import get_object_or_404, redirect, render
 
 from contact.models import Contact
 
 
 def index(request):
     contacts = Contact.objects.filter(show=True).order_by('-id')
-    context = {'contacts': contacts,}
+    context = {'contacts': contacts, 'site_title': 'Contatos - '}
 
     return render(request,'contact/index.html', context)
 
+def search(request):
+    search_value = request.GET.get('q', '').strip()
+
+    if search_value == '':
+        return redirect('contact: index')
+    
+    contacts = Contact.objects.filter(show=True).filter(
+        Q(AKA__icontains=search_value) |
+        Q(name__icontains=search_value) |
+        Q(phone__icontains=search_value) |
+        Q(email__icontains=search_value)
+        ).order_by('-id')
+    
+    context = {'contacts': contacts, 'site_title': 'Search - '}
+
+    return render(request, 'contact/index.html', context)
+
 def contact(request, contact_id):
     single_contact = get_object_or_404(Contact, pk=contact_id, show=True)
-    context = {'contact': single_contact}
+    site_title = f'{single_contact.AKA} - '
+    context = {'contact': single_contact, 'site_title': site_title}
     return render(request, 'contact/contact.html', context)
