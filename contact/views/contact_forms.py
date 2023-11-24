f7b599e573f46cac8fc4ea812ca48257d28442f7
Vinicius Ouro Preto Ribeiro <viniciusouro73@gmail.com>
2023-11-24 15:31:19 -0300

    Modificações: login/logout, authentications, form para atualizar users, etc

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,4 +1,5 @@
 
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse
 
@@ -6,6 +7,7 @@ from contact.forms import ContactForm
 from contact.models import Contact
 
 
+@login_required(login_url='contact:login')
 def create(request):
     form_action = reverse('contact:create')
 
@@ -14,15 +16,18 @@ def create(request):
         context = {'form': form, 'form_action': form_action}
 
         if form.is_valid():
-            contact = form.save()
+            contact = form.save(commit=False)
+            contact.owner = request.user
+            contact.save()
             return redirect('contact:update', contact_id=contact.pk)
    
         return render(request, 'contact/create.html', context)
     context = {'form': ContactForm(), 'form_action': form_action}
     return render(request, 'contact/create.html', context)
 
+@login_required(login_url='contact:login')
 def update(request, contact_id):
-    contact = get_object_or_404(Contact, pk=contact_id, show=True)
+    contact = get_object_or_404(Contact, pk=contact_id, show=True, owner=request.user)
     form_action = reverse('contact:update', args=(contact_id,))
 
     if request.method == 'POST':
@@ -38,8 +43,9 @@ def update(request, contact_id):
     context = {'form': ContactForm(instance=contact), 'form_action': form_action,}
     return render(request, 'contact/create.html', context)
 
+@login_required(login_url='contact:login')
 def delete(request, contact_id):
-    contact = get_object_or_404(Contact, pk=contact_id, show=True)
+    contact = get_object_or_404(Contact, pk=contact_id, show=True, owner=request.user)
     confirmation = request.POST.get('confirmation', 'no')
 
     if confirmation == 'yes':
--- contact/views/user_forms.py
+++ contact/views/user_forms.py
@@ -1,7 +1,9 @@
-from django.contrib import messages
+from django.contrib import auth, messages
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm
 from django.shortcuts import redirect, render
 
-from contact.forms import RegisterForm
+from contact.forms import RegisterForm, RegisterUpdateForm
 
 
 def register(request):
@@ -12,6 +14,40 @@ def register(request):
         if form.is_valid():
             form.save()
             messages.success(request, 'Registered User')
-            return redirect('contact:index')
+            return redirect('contact:login')
     
     return render(request, 'contact/register.html', {'form': form})
+
+@login_required(login_url='contact:login')
+def user_update(request):
+    form = RegisterUpdateForm(instance=request.user)
+
+    if request.method != 'POST':
+        return render(request, 'contact/user_update.html', {'form': form})
+    form = RegisterUpdateForm(data=request.POST, instance=request.user)
+
+    if not form.is_valid():
+        return render(request, 'contact/user_update.html', {'form': form})
+    form.save()
+    return redirect('contact:user_update')
+    
+
+
+def login_view(request):
+    form = AuthenticationForm(request)
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+
+        if form.is_valid():
+            user = form.get_user()
+            auth.login(request, user)
+            messages.success(request, 'Login Success')
+            return redirect('contact:index')
+        messages.error(request, 'Invalid Login')
+    
+    return render(request, 'contact/login.html', {'form': form})
+
+@login_required(login_url='contact:login')
+def logout_view(request):
+    auth.logout(request)
+    return redirect('contact:login')
